d77ba47fd8dc4cc7211d603299aeb0bbc072e05d
iproman <itechman@yandex.ru>
2021-09-15 12:58:31 +0500

    Get user from selected

--- src/app/views/read/read.component.ts
+++ src/app/views/read/read.component.ts
@@ -1,13 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { UserStateService } from '../../store/user-state.service';
+import { UserInterface } from '../../store/user-interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-read',
   templateUrl: './read.component.html',
-  styleUrls: ['./read.component.css']
+  styleUrls: ['./read.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ReadComponent implements OnInit {
 
-  constructor() {
+  user$: Observable<UserInterface | undefined> = this.userStateService.selectedUser$;
+
+  constructor(
+    private readonly userStateService: UserStateService
+  ) {
   }
 
   ngOnInit(): void {
