ec37661ed929b38d66a0a45ff46379b0b71095b2
iproman <itechman@yandex.ru>
2021-09-15 12:19:48 +0500

    Add addition patsh

--- src/app/app-router.module.ts
+++ src/app/app-router.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './views/home/home.component';
+import { CreateComponent } from './views/create/create.component';
+import { UpdateComponent } from './views/update/update.component';
+import { ReadComponent } from './views/read/read.component';
+import { DeleteComponent } from './views/delete/delete.component';
 
 
 const routes: Routes = [
@@ -12,6 +16,22 @@ const routes: Routes = [
   {
     path: 'home',
     component: HomeComponent,
+  },
+  {
+    path: 'create',
+    component: CreateComponent,
+  },
+  {
+    path: 'update/:id',
+    component: UpdateComponent,
+  },
+  {
+    path: 'read/:id',
+    component: ReadComponent,
+  },
+  {
+    path: 'delete/:id',
+    component: DeleteComponent,
   }
 ]
 
