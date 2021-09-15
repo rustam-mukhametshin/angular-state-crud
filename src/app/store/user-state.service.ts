56fba11520c13b30cce3772d85622b974f2be6c9
iproman <itechman@yandex.ru>
2021-09-15 11:04:08 +0500

    Multicasting for observable

--- src/app/store/user-state.service.ts
+++ src/app/store/user-state.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { StateService } from './state.service';
 import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { UserInterface } from './user-interface';
 import { UserStateInterface } from './user-state-interface';
 
@@ -14,7 +15,11 @@ const initialState: UserStateInterface = {
   providedIn: 'root'
 })
 export class UserStateService extends StateService<UserStateInterface> {
-  users$: Observable<UserInterface[]> = this.select(state => state.users);
+  users$: Observable<UserInterface[]> = this.select(state => state.users)
+    .pipe(
+      shareReplay({refCount: true, bufferSize: 1})
+    )
+  ;
 
   selectedUser$: Observable<UserInterface | undefined> = this.select(state => {
     return state.users.find(user => user.id === state.selectedUserId)
