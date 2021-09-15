0375ef45810c7dec33a6c963021e8faf3f62826e
iproman <itechman@yandex.ru>
2021-09-15 13:23:19 +0500

    Rename form

--- src/app/views/create/create.component.ts
+++ src/app/views/create/create.component.ts
@@ -9,14 +9,14 @@ import { UserStateService } from '../../store/user-state.service';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
-  userForm: FormGroup;
+  form: FormGroup;
   disabled: boolean = false;
   user: UserInterface | undefined;
 
   constructor(
     private readonly userStateService: UserStateService
   ) {
-    this.userForm = new FormGroup({
+    this.form = new FormGroup({
       id: new FormControl('', [Validators.required]),
       username: new FormControl('', [Validators.required]),
       email: new FormControl('', [Validators.email, Validators.required]),
@@ -32,13 +32,13 @@ export class CreateComponent implements OnInit {
   }
 
   submit() {
-    if (this.userForm.errors) {
+    if (this.form.errors) {
       return;
     }
     // Todo: UUID
     const newUser: UserInterface = {
       id: 123,
-      ...this.userForm.value,
+      ...this.form.value,
     } as UserInterface;
 
     this.userStateService.create(newUser);
