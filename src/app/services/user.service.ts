6c0422cc32beb11b75293a87f6f08045eeb77393
iproman <itechman@yandex.ru>
2021-09-15 12:07:47 +0500

    Add user service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserInterface as User } from '../store/user-interface';
+
+const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+    private readonly httpClient: HttpClient
+  ) {
+  }
+
+  getUsers(): Observable<User[]> {
+    return this.httpClient.get<User[]>(apiUrl);
+  }
+
+  createUser(user: User): Observable<User> {
+    return this.httpClient.post<User>(apiUrl, user);
+  }
+
+  updateUser(user: User): Observable<User> {
+    return this.httpClient.put<User>(apiUrl + user.id, user);
+  }
+
+  deleteUser(user: User): Observable<void> {
+    return this.httpClient.delete<void>(apiUrl + user.id);
+  }
+}
