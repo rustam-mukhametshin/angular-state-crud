602a359996b096694b8ae6bda9a28790fa029670
iproman <itechman@yandex.ru>
2021-09-15 13:11:08 +0500

    Add create form function

--- src/app/views/create/create.component.ts
+++ src/app/views/create/create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { UserInterface } from '../../store/user-interface';
 
 @Component({
   selector: 'app-create',
@@ -6,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  userForm: FormGroup;
+  disabled: boolean = false;
+  user: UserInterface | undefined;
 
   constructor() {
+    this.userForm = new FormGroup({
+      id: new FormControl('', [Validators.required]),
+      username: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      phone: new FormControl('', [Validators.required])
+    })
   }
 
   ngOnInit(): void {
   }
 
+  reset() {
+
+  }
+
+  submit() {
+
+  }
 }
