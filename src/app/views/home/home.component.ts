d36d58ef20d49c1d2ef313a7782fdb2daf0600bc
iproman <itechman@yandex.ru>
2021-09-15 11:05:05 +0500

    Get users from state in c

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -1,16 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { UserStateService } from '../../store/user-state.service';
+import { UserInterface } from '../../store/user-interface';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeComponent implements OnInit {
 
-  constructor() {
+  users: UserInterface[] | undefined;
+
+  labels: string[] | undefined;
+
+  constructor(
+    private readonly userStateService: UserStateService,
+    private readonly cdr: ChangeDetectorRef
+  ) {
+    this.userStateService.users$.subscribe(users => {
+      this.users = users;
+      cdr.markForCheck();
+    }) // Изменить на async pipe, все равно вызывает markForCheck();
   }
 
   ngOnInit(): void {
+    this.labels = [
+      'ID',
+      'Username',
+      'Email',
+      'Phone'
+    ]
   }
 
 }
