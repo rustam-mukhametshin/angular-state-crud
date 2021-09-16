7ab4f81ff085f6f921a07e66c6d4fdedbb27b1c7
iproman <itechman@yandex.ru>
2021-09-16 15:53:38 +0500

    Return observable

--- src/app/store/user-state.service.ts
+++ src/app/store/user-state.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { StateService } from './state.service';
 import { Observable } from 'rxjs';
-import { shareReplay } from 'rxjs/operators';
+import { shareReplay, tap } from 'rxjs/operators';
 import { UserInterface } from './user-interface';
 import { UserStateInterface } from './user-state-interface';
 import { UserService } from '../services/user.service';
@@ -51,32 +51,41 @@ export class UserStateService extends StateService<UserStateInterface> {
 
   // Service API
 
-  protected create(user: UserInterface) {
-    this.userService.createUser(user).subscribe((newUser) => {
-      this.setState({
-        users: [...this.state.users, newUser],
-        selectedUserId: newUser.id,
-      });
-    });
+  protected create(user: UserInterface): Observable<UserInterface> {
+    return this.userService.createUser(user)
+      .pipe(
+        tap(
+          newUser => {
+            this.setState({
+              users: [...this.state.users, newUser],
+              selectedUserId: newUser.id,
+            })
+          }
+        )
+      );
   }
 
-  protected delete(user: UserInterface | undefined) {
-    if (user) {
-      this.userService.deleteUser(user).subscribe(() => {
-        this.setState({
-          selectedUserId: undefined,
-          users: this.state.users.filter((item) => item.id !== user.id),
-        });
-      });
-    }
+  protected delete(user: UserInterface | undefined): Observable<void> {
+    return this.userService.deleteUser(user)
+      .pipe(
+        tap(() => {
+          this.setState({
+            selectedUserId: undefined,
+            users: this.state.users.filter((item) => item.id !== (user ? user.id : null)),
+          });
+        })
+      );
   }
 
-  protected update(user: UserInterface) {
-    this.userService.updateUser(user).subscribe((updatedUser) => {
-      this.setState({
-        users: this.state.users.map((item) => (item.id === user.id ? updatedUser : item)),
-      });
-    });
+  protected update(user: UserInterface): Observable<UserInterface> {
+    return this.userService.updateUser(user)
+      .pipe(
+        tap(updatedUser => {
+          this.setState({
+            users: this.state.users.map((item) => (item.id === user.id ? updatedUser : item)),
+          });
+        })
+      );
   }
 
   private init() {
