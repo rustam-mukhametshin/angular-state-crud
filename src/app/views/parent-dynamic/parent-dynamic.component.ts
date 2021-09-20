7209750755104d3e2cf64b3fea89d9699d6af8f1
iproman <itechman@yandex.ru>
2021-09-20 14:46:16 +0500

    Show dynamic child

--- src/app/views/parent-dynamic/parent-dynamic.component.ts
+++ src/app/views/parent-dynamic/parent-dynamic.component.ts
@@ -1,16 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ComponentFactoryResolver, Inject, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { ShowDynamicDirective } from '../../directives/show-dynamic.directive';
+import { ChildDynamicComponent } from '../child-dynamic/child-dynamic.component';
+import { PAGE_VISIBILITY } from '../../tokens/page-visibility.token';
+import { Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-parent-dynamic',
   templateUrl: './parent-dynamic.component.html',
   styleUrls: ['./parent-dynamic.component.css']
 })
-export class ParentDynamicComponent implements OnInit {
+export class ParentDynamicComponent implements OnInit, OnDestroy {
 
-  constructor() {
+  @ViewChild(ShowDynamicDirective, {static: true}) showDynamicDirective!: ShowDynamicDirective;
+  private unsubscribe$: Subject<null> = new Subject<null>();
+
+  constructor(
+    private componentFactoryResolver: ComponentFactoryResolver,
+    @Inject(PAGE_VISIBILITY) private readonly pageVisibility: Observable<boolean>
+  ) {
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.pageVisibility
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(visible => {
+        if (visible) {
+          this.loadComponent();
+          console.log('Show child: ', visible)
+        } else {
+          console.log('Hide child: ', visible);
+          this.loadComponent(false);
+        }
+      })
+  }
+
+  loadComponent(check: boolean = true) {
+    const componentFactory = this.componentFactoryResolver.resolveComponentFactory<ChildDynamicComponent>(ChildDynamicComponent);
+
+    const viewContainerRef = this.showDynamicDirective.viewContainerRef;
+    viewContainerRef.clear();
+    // if (check) {
+    const componentRef = viewContainerRef.createComponent<ChildDynamicComponent>(componentFactory);
+    console.log(componentRef);
+    componentRef.instance.title = `Child - Wow here is dynamic data ${Date.now()}`;
+    // } else {
+    //   viewContainerRef.clear();
+    // }
+  }
+
+  ngOnDestroy() {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
   }
 
 }
