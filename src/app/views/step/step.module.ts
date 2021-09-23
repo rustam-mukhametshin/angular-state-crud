a7736c09514df9175a0e3469de6757cd6296b3c1
iproman <itechman@yandex.ru>
2021-09-23 14:56:00 +0500

    Include addition modules

--- src/app/views/step/step.module.ts
+++ src/app/views/step/step.module.ts
@@ -3,23 +3,32 @@ import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
 import { StepRouterModule } from './step.router.module';
 import { SharedModule } from '../../modules/shared.module';
-
-const modules = [
-  SharedModule,
-  StepRouterModule,
-]
-
-const components = [
-  FormComponent,
-  ListComponent,
-];
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatStepperModule } from '@angular/material/stepper';
+import { FormlyBootstrapModule } from '@ngx-formly/bootstrap';
+import { FormlyModule } from '@ngx-formly/core';
+import { FormlyFieldStepper } from './form/stepper.type';
 
 @NgModule({
   declarations: [
-    ...components,
+    FormComponent,
+    ListComponent,
+    FormlyFieldStepper,
   ],
   imports: [
-    ...modules,
+    SharedModule,
+    StepRouterModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    FormlyBootstrapModule,
+    FormlyModule.forRoot({
+      validationMessages: [
+        {name: 'required', message: 'This field is required'},
+      ],
+      types: [
+        {name: 'stepper', component: FormlyFieldStepper, wrappers: []},
+      ],
+    }),
   ]
 })
 export class StepModule {
