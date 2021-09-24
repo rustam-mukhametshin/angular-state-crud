4f749b28be4fb06b9864e4ffc73797a95964328c
iproman <itechman@yandex.ru>
2021-09-24 13:52:58 +0500

    Addition fields for interface

--- src/app/views/step/move-copy.interface.ts
+++ src/app/views/step/move-copy.interface.ts
@@ -1,9 +1,26 @@
 export interface MoveCopyInterface {
   contextName: string;
   contextID: number;
-  serverName: string;
+  serverNameFirst: string;
   requestDate: Date,
   requestor: string;
+
   typeOfRequest: string;
   executionDateTime: Date;
+
+  turnOfAlertsOnSourceContext: string,
+  setSourceDatabaseToLocked: string,
+
+  selectBackupDateTime: string,
+  destinationServer: string,
+  inboundEmailUniqueName: string,
+  targetContextName: string,
+  serverName: string,
+  includeHistory: string,
+  migrationOfSourceDocumentsIncluded: string,
+  copyUserAccountsOfSource: string,
+  serverWideAccess: string,
+
+  lastInfo: string,
+  lastInfo2: string,
 }
