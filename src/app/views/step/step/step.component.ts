00b4a39cb0aa505785e3abbf13a6ac66fe0c7624
iproman <itechman@yandex.ru>
2021-09-24 13:02:31 +0500

    Separate step c

--- src/app/views/step/step/step.component.ts
+++ src/app/views/step/step/step.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FieldType, FormlyFieldConfig } from '@ngx-formly/core';
 
 @Component({
   selector: 'app-step',
   templateUrl: './step.component.html',
   styleUrls: ['./step.component.css']
 })
-export class StepComponent implements OnInit {
+export class StepComponent extends FieldType implements OnInit {
 
-  constructor() {
+  ngOnInit() {
+    console.log('Step c :', this.field?.fieldGroup)
   }
 
-  ngOnInit(): void {
+
+  // @ts-ignore
+  isValid(field: FormlyFieldConfig) {
+    // console.log(field.key);
+    if (field.key) {
+      // @ts-ignore
+      return field.formControl.valid;
+    }
+
+    // @ts-ignore
+    return field.fieldGroup.every(f => this.isValid(f));
+  }
+
+  checkEditable(index: number, step: any): boolean {
+    // console.log(step);
+
+    /*if (field.key) {
+      // @ts-ignore
+      return field.formControl.valid;
+    }*/
+    return true;
+  }
+
+  checkModelEmpty(obj: {}) {
+    return Object.values(obj).filter(v => !!v)
   }
 
 }
