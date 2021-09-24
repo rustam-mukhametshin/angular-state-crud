5b87bdf9a176151c2b0e1d565540cdf7ac1ceb77
iproman <itechman@yandex.ru>
2021-09-24 12:33:10 +0500

    Add interface MoveCopyInterface

---
+++ src/app/views/step/move-copy.interface.ts
@@ -0,0 +1,9 @@
+export interface MoveCopyInterface {
+  contextName: string;
+  contextID: number;
+  serverName: string;
+  requestDate: Date,
+  requestor: string;
+  typeOfRequest: string;
+  executionDateTime: Date;
+}
