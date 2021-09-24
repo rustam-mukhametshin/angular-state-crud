612c3c0ebc698f3b8210a5f6629f843b767ab167
iproman <itechman@yandex.ru>
2021-09-24 14:02:32 +0500

    Separate step page

--- src/app/views/step/step-page/step-page.component.ts
+++ src/app/views/step/step-page/step-page.component.ts
@@ -1,16 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
+import { FormlyFieldConfigCustom } from '../formly-field-config';
 
 @Component({
   selector: 'app-step-page',
   templateUrl: './step-page.component.html',
   styleUrls: ['./step-page.component.css']
 })
-export class StepPageComponent implements OnInit {
+export class StepPageComponent {
+
+  form = new FormGroup({});
+  mainModel: any;
+
+  options: FormlyFormOptions = {};
+
+  fields!: FormlyFieldConfig[];
 
   constructor() {
+    this.mainModel = new FormlyFieldConfigCustom;
+    this.fields = this.mainModel.fields;
   }
 
-  ngOnInit(): void {
+
+  submit() {
+    // alert(JSON.stringify(this.mainModel.model));
   }
 
 }
