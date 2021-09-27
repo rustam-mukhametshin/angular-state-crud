8c07906dd4fe44f232bec66c2d01aa374d50e52a
iproman <itechman@yandex.ru>
2021-09-27 15:01:53 +0500

    Add tests for init method

--- src/app/services/step.service.spec.ts
+++ src/app/services/step.service.spec.ts
@@ -2,15 +2,32 @@ import { TestBed } from '@angular/core/testing';
 
 import { StepService } from './step.service';
 
-describe('StepService', () => {
+fdescribe('StepService', () => {
   let service: StepService;
+  const fixtureData = {
+    key: 'STATE_CONFIG_STEP',
+    value: 'someValue'
+  };
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(StepService);
   });
 
+  beforeEach(() => {
+    // Clear storage
+    service.clear();
+  });
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('#init should init configs', done => {
+    // Todo: Check also saved data
+    service.init().subscribe(configs => {
+      expect(configs).toBeNull();
+      done();
+    });
   });
 });
