79e439501b8faf59b9609a5349ca64917a6305c2
iproman <itechman@yandex.ru>
2021-09-27 14:55:34 +0500

    Addition methods to work with storage

--- src/app/services/step.service.ts
+++ src/app/services/step.service.ts
@@ -1,10 +1,22 @@
 import { Injectable } from '@angular/core';
+import { StepInterface } from '../interfaces/step.interface';
+
+const CONFIG_KEY = 'STATE_CONFIG_STEP'
 
 @Injectable({
   providedIn: 'root'
 })
 export class StepService {
 
-  constructor() {
+  private get config(): StepInterface {
+    return JSON.parse(<string>localStorage.getItem(CONFIG_KEY));
+  }
+
+  private set config(data: StepInterface) {
+    localStorage.setItem(CONFIG_KEY, JSON.stringify(data));
+  }
+
+  clear() {
+    localStorage.removeItem(CONFIG_KEY);
   }
 }
