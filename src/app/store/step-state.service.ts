cacb91a9df3e1550c2f8eced075dfbbea9b7d533
iproman <itechman@yandex.ru>
2021-10-04 01:20:45 +0500

    Init initial data

--- src/app/store/step-state.service.ts
+++ src/app/store/step-state.service.ts
@@ -3,11 +3,27 @@ import { StateService } from './state.service';
 import { StepStateInterface } from '../interfaces/step-state.interface';
 import { Observable, of } from 'rxjs';
 import { StepInterface } from '../interfaces/step.interface';
+import { shareReplay } from 'rxjs/operators';
+
+const initialState: StepStateInterface = {
+  configs: [],
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class StepStateService extends StateService<StepStateInterface> {
+
+  // Todo: why array?
+  configs$: Observable<StepInterface[]> = this.select(state => state.configs)
+    .pipe(
+      shareReplay({refCount: true, bufferSize: 1})
+    );
+
+  constructor() {
+    super(initialState);
+  }
+
 
   init(): Observable<unknown> {
     return of();
