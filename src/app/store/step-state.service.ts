33072d345916f755a3d7e3fd65f12e42f80f4a6a
iproman <itechman@yandex.ru>
2021-10-04 00:56:36 +0500

    Add initial methods of stepState

--- src/app/store/step-state.service.ts
+++ src/app/store/step-state.service.ts
@@ -1,10 +1,23 @@
 import { Injectable } from '@angular/core';
+import { StateService } from './state.service';
+import { StepStateInterface } from '../interfaces/step-state.interface';
+import { Observable, of } from 'rxjs';
+import { StepInterface } from '../interfaces/step.interface';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StepStateService {
+export class StepStateService extends StateService<StepStateInterface> {
 
-  constructor() {
+  init(): Observable<unknown> {
+    return of();
+  }
+
+  update(config: any): Observable<StepInterface> {
+    return of()
+  }
+
+  getConfig(): Observable<StepInterface> {
+    return of()
   }
 }
