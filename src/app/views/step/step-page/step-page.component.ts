74c4437357254fd275c493ed1f5860c0a5101048
iproman <itechman@yandex.ru>
2021-10-04 02:28:53 +0500

    Get configs from facade

--- src/app/views/step/step-page/step-page.component.ts
+++ src/app/views/step/step-page/step-page.component.ts
@@ -2,6 +2,10 @@ import { Component, ViewEncapsulation } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { FormlyFieldConfigCustom } from '../formly-field-config';
+import { FacadeService } from '../../../store/facade.service';
+import { Observable } from 'rxjs';
+import { StepInterface } from '../../../interfaces/step.interface';
+import { StepEnum } from '../../../enums/step-enum';
 
 @Component({
   selector: 'app-step-page',
@@ -18,7 +22,13 @@ export class StepPageComponent {
 
   fields!: FormlyFieldConfig[];
 
-  constructor() {
+  configs$: Observable<StepInterface>;
+
+  constructor(
+    private readonly facadeService: FacadeService,
+  ) {
+    this.configs$ = this.facadeService.step(StepEnum.getConfigs);
+
     this.mainModel = new FormlyFieldConfigCustom;
     this.fields = this.mainModel.fields;
   }
