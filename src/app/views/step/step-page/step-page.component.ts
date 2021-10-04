d278984d85b547b7dd8290922fdfb14f537ba149
iproman <itechman@yandex.ru>
2021-10-04 16:09:28 +0500

    Get dynamic ID

--- src/app/views/step/step-page/step-page.component.ts
+++ src/app/views/step/step-page/step-page.component.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { StepInterface } from '../../../interfaces/step.interface';
 import { StepEnum } from '../../../enums/step-enum';
 import { take } from 'rxjs/operators';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-step-page',
@@ -17,6 +18,7 @@ export class StepPageComponent implements OnInit {
 
   constructor(
     private readonly facadeService: FacadeService,
+    private readonly activatedRoute: ActivatedRoute
   ) {
 
   }
@@ -31,9 +33,10 @@ export class StepPageComponent implements OnInit {
 
   private setInitialValues() {
     // Todo: Change to ActivatedRoute
+    const id = this.activatedRoute.snapshot.paramMap.get('id') || '0';
     const obs: StepInterface = {
       contextName: 'Demo Sales NL',
-      contextID: 47,
+      contextID: parseInt(id, 10),
       serverNameFirst: 'DEV3',
       requestDate: '09/22/2021',
       requestor: 'First Last name',
