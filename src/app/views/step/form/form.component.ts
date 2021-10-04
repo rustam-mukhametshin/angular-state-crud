8abeaada69625e2531164c4017b65c57b3147f64
iproman <itechman@yandex.ru>
2021-10-04 14:26:09 +0500

    Init configs

--- src/app/views/step/form/form.component.ts
+++ src/app/views/step/form/form.component.ts
@@ -1,19 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { FormlyFieldConfig, FormlyFormOptions } from '@ngx-formly/core';
 import { FormlyFieldConfigCustom } from '../formly-field-config';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 import { StepInterface } from '../../../interfaces/step.interface';
+import { takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
 })
-export class FormComponent implements OnInit {
-
+export class FormComponent implements OnInit, OnDestroy {
   @Input()
   configs$!: Observable<StepInterface>;
+  configs!: StepInterface;
 
   form = new FormGroup({});
   mainModel: any;
@@ -21,9 +22,22 @@ export class FormComponent implements OnInit {
   initialFormConfigs!: FormlyFieldConfig[];
   options: FormlyFormOptions = {};
 
+  private subs$ = new Subject<void>();
+
   ngOnInit() {
     this.mainModel = new FormlyFieldConfigCustom;
     this.initialFormConfigs = this.mainModel.initialFormConfigs;
+
+    this.configs$
+      .pipe(takeUntil(this.subs$))
+      .subscribe(data => {
+        this.configs = data;
+      })
+  }
+
+  ngOnDestroy() {
+    this.subs$.next();
+    this.subs$.complete();
   }
 
   submit() {
