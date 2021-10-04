4ab19479055a44c408ac2b2a00061aad2ee79983
iproman <itechman@yandex.ru>
2021-10-04 16:19:46 +0500

    Get dynamic initial data

--- src/app/views/step/step-page/step-page.component.ts
+++ src/app/views/step/step-page/step-page.component.ts
@@ -3,8 +3,9 @@ import { FacadeService } from '../../../store/facade.service';
 import { Observable } from 'rxjs';
 import { StepInterface } from '../../../interfaces/step.interface';
 import { StepEnum } from '../../../enums/step-enum';
-import { take } from 'rxjs/operators';
+import { switchMap, take } from 'rxjs/operators';
 import { ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-step-page',
@@ -18,7 +19,8 @@ export class StepPageComponent implements OnInit {
 
   constructor(
     private readonly facadeService: FacadeService,
-    private readonly activatedRoute: ActivatedRoute
+    private readonly activatedRoute: ActivatedRoute,
+    private readonly httpClient: HttpClient
   ) {
 
   }
@@ -32,19 +34,26 @@ export class StepPageComponent implements OnInit {
   }
 
   private setInitialValues() {
-    // Todo: Change to ActivatedRoute
+
     const id = this.activatedRoute.snapshot.paramMap.get('id') || '0';
     const currentDate = new Date().toLocaleString().split(',')[0];
 
-    const obs: StepInterface = {
-      contextName: 'Demo Sales NL',
-      contextID: parseInt(id, 10),
-      serverNameFirst: 'DEV3',
-      requestDate: currentDate,
-      requestor: 'First Last name',
-    }
+    const api_url = `https://jsonplaceholder.typicode.com/users/${id}`;
 
-    return this.facadeService.step(StepEnum.updateConfigs, obs);
+    return this.httpClient.get(api_url)
+      .pipe(
+        switchMap((user: any) => {
+          const obs: StepInterface = {
+            contextName: user.address.city,
+            contextID: parseInt(id, 10),
+            serverNameFirst: user.website,
+            requestDate: currentDate,
+            requestor: user.name,
+          }
+
+          return this.facadeService.step(StepEnum.updateConfigs, obs);
+        })
+      )
   }
 
 }
